d2c95627640d287635e15efad9f9271a9ae27bf0
cutebaby0630 <lovemath0630@gmail.com>
2018-11-13 15:42:34 +0800

    讀取資料

--- app/src/main/java/com/cutebaby/atm/Login.java
+++ app/src/main/java/com/cutebaby/atm/Login.java
@@ -9,19 +9,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private String username;
+    private String password;
+    private EditText edname;
+    private EditText edpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        String name =getSharedPreferences("atm",MODE_PRIVATE)
+                .getString("USERID","");
+//        EditText edname = findViewById(R.id.ed_name);
+      findView();
+        edname.setText(name);
     }
     public void login(View view){
-        final EditText edname = findViewById(R.id.ed_name);
-        final EditText edpassword = findViewById(R.id.ed_password);
-        String username = edname.getText().toString();
-        String password = edpassword.getText().toString();
+        findView();
+        username = edname.getText().toString();
+        password = edpassword.getText().toString();
         if("cutebaby".equals(username)&& "love0420".equals(password) ){
             setResult(RESULT_OK);
 //            登入成功後，儲存資料
@@ -29,25 +39,31 @@ public class Login extends AppCompatActivity {
 //            setting.edit().putString("USERID",username).apply();
             getSharedPreferences("atm",MODE_PRIVATE)
                     .edit()
-                    .putString("USERID",username)
+                    .putString("USERID", username)
                     .apply();
             finish();
         }else{
             new AlertDialog.Builder(this).setMessage("登入失敗")
                     .setTitle("error")
                     .setPositiveButton("OK",null)
-                    .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            edname.setText("");
-                            edpassword.setText("");
-                        }
-                    }).show();
+//                    .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            edname.setText("");
+//                            edpassword.setText("");
+//                        }
+//                    })
+        .show();
 
         }
 
 
 
+    }
+
+    private void findView() {
+        edname = findViewById(R.id.ed_name);
+        edpassword = findViewById(R.id.ed_password);
     }
 
 }
