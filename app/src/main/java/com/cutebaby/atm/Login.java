b5abafe9bef0c3092688bb25082c69246ffcfbf8
cutebaby0630 <lovemath0630@gmail.com>
2018-11-13 15:16:31 +0800

    SharedPreferences

--- app/src/main/java/com/cutebaby/atm/Login.java
+++ app/src/main/java/com/cutebaby/atm/Login.java
@@ -3,6 +3,7 @@ package com.cutebaby.atm;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,6 +24,13 @@ public class Login extends AppCompatActivity {
         String password = edpassword.getText().toString();
         if("cutebaby".equals(username)&& "love0420".equals(password) ){
             setResult(RESULT_OK);
+//            登入成功後，儲存資料
+//            SharedPreferences setting = getSharedPreferences("atm",MODE_PRIVATE);
+//            setting.edit().putString("USERID",username).apply();
+            getSharedPreferences("atm",MODE_PRIVATE)
+                    .edit()
+                    .putString("USERID",username)
+                    .apply();
             finish();
         }else{
             new AlertDialog.Builder(this).setMessage("登入失敗")
