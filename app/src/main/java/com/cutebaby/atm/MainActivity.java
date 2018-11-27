e46d7b291c6cfe01b314e170b6325c70fb8b5fae
cutebaby0630 <lovemath0630@gmail.com>
2018-11-27 16:15:13 +0800

    RecyclerView layout

--- app/src/main/java/com/cutebaby/atm/MainActivity.java
+++ app/src/main/java/com/cutebaby/atm/MainActivity.java
@@ -3,19 +3,38 @@ package com.cutebaby.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
     private static final int RC_LOGIN = 100;
-    boolean login = false;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (!login){
-            Intent intent = new Intent(this,Login.class);
+            boolean login = false;
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                if (!login){
+                    Intent intent = new Intent(this,Login.class);
             startActivityForResult(intent,RC_LOGIN);
         }
+//        listView
+//                listView();
+//                RecyclerView
     }
+
+    private void listView() {
+        List<String> fruits = Arrays.asList("蘋果","芭樂","香蕉");
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
